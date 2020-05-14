254cf68ec9d08cacac2f09fc2bc7b5e85aa22a2a
Leo Holman <lah2719@g.rit.edu>
2020-05-14 14:59:48 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='RubiksCube',
+    version='0.1dev',
+    packages='rubiks_cube',
+    license='GNU General Public License',
+    long_description=open('README.md').read(),
+    author='Leo Holman'
+)
